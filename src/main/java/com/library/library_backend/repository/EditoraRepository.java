47e291ae959f102daceeec4ddd72dc39555af5be
Guilherme <guilhermealvarenga0124@gmail.com>
2025-10-08 14:21:34 -0300

    Adicionados os DTOs de editora. Implementadas as classes EditoraGetDTO, Editora e EditoraRepository.

--- src/main/java/com/library/library_backend/repository/EditoraRepository.java
+++ src/main/java/com/library/library_backend/repository/EditoraRepository.java
@@ -1,5 +1,7 @@
 package com.library.library_backend.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +10,7 @@ import com.library.library_backend.model.Editora;
 
 @Repository
 public interface EditoraRepository extends JpaRepository <Editora, Integer>{
-    
+        Optional<Editora> findByNome(String nome);
+        Optional<Editora> findByCNPJ(String cnpj);
+        Optional<Editora> findByGerente(String gerente);
 }
