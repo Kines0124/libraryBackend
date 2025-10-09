63584e71f5bcf76bc7ef2ade8ce42cea3c466dd4
Guilherme <guilhermealvarenga0124@gmail.com>
2025-10-09 13:58:09 -0300

    Implementado os metodos de Put e Delete para a tabela de Editora

--- src/main/java/com/library/library_backend/controller/EditoraController.java
+++ src/main/java/com/library/library_backend/controller/EditoraController.java
@@ -6,9 +6,11 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +18,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 import com.library.library_backend.dto.EditoraGetDTO;
 import com.library.library_backend.dto.EditoraPostDTO;
+import com.library.library_backend.dto.EditoraPutDTO;
 import com.library.library_backend.model.Editora;
 import com.library.library_backend.repository.EditoraRepository;
 
@@ -31,7 +34,6 @@ public class EditoraController {
 	
 	@GetMapping
 	public List<Editora> get(){
-		ResponseEntity<Editora> ret = ResponseEntity.notFound().build();
 		List<Editora> list = repository.findAll();
 		return list;
 	}
@@ -54,7 +56,7 @@ public class EditoraController {
 	public ResponseEntity<EditoraGetDTO> post (@RequestBody @Valid EditoraPostDTO body, UriComponentsBuilder uriBuilder){
 		ResponseEntity<EditoraGetDTO> ret = ResponseEntity.unprocessableEntity().build();
 		Editora item = body.convert();
-		Optional<Editora> search = repository.findById(item.getId());
+		Optional<Editora> search = repository.findByNome(item.getNome());
 		if(!search.isPresent()) {
 			repository.save(item);
 			URI uri = uriBuilder.path("/editora/{id}").buildAndExpand(item.getId()).toUri();
@@ -65,4 +67,46 @@ public class EditoraController {
 		return ret;
 	}
 	
+	@PutMapping("/{id}")
+	@Transactional
+	public ResponseEntity<EditoraGetDTO> put(@PathVariable("id") int id, @RequestBody @Valid EditoraPutDTO body, UriComponentsBuilder uriBuilder){
+		ResponseEntity<EditoraGetDTO> ret =  ResponseEntity.notFound().build();
+		Optional<Editora> search = repository.findById(id);
+		if (search.isPresent()){
+			Editora item = search.get();
+			boolean found = false;
+			Optional<Editora> other = repository.findByCnpj(body.getCnpj());
+			if(other.isPresent()){
+				Editora old = other.get();
+				if(item.getId() != old.getId()){
+					found = true;
+					System.out.println("Ja existe editora com esse CNPJ...");
+					ret = ResponseEntity.unprocessableEntity().build();
+				}
+			}
+			if(!found){
+				body.update(item);
+				URI uri = uriBuilder.path("/livro/{id}").buildAndExpand(item.getId()).toUri();
+				ret = ResponseEntity.created(uri).body(new EditoraGetDTO(item));
+			}
+		}else{
+			System.out.println("Editora não encontrada...");
+		}
+		return ret;
+	}
+
+	@DeleteMapping("/{id}")
+	@Transactional
+	public ResponseEntity<?> delete (@PathVariable("id") int id){
+		ResponseEntity<Editora> ret = ResponseEntity.notFound().build();
+		Optional<Editora> search = repository.findById(id);
+		if(search.isPresent()){
+			Editora item = search.get();
+			repository.delete(item);
+			ret = ResponseEntity.ok().build();
+		} else {
+			System.out.println("Editora não encontrada...");
+		}
+		return ret;
+	}
 }
